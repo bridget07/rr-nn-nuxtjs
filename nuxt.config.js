8e1fff069e7784c7ed2e3e4e9258d7292da89241
gaoshuang@360shop.cn <865967113@qq.com>
2021-02-21 11:06:13 +0800

    修改nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,10 @@ export default {
             })
         }
     },
+    server: {
+        host: '0.0.0.0',
+        port: 3000
+    },
     plugins: [
         '~/plugins/request.js',
         '~/plugins/dayjs.js',
